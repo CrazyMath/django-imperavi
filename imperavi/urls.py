a6d707ccadee0c6f31cb4da1abdb75148ac297b9
Konstantin <moiseenko@steelkiwi.com>
2013-04-17 13:12:30 +0300

    Fix for django v1.3. Change widget.

--- imperavi/urls.py
+++ imperavi/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls.defaults import patterns, url
 
 from views import upload_image, uploaded_images_json, upload_file
 
--- imperavi/widget.py
+++ imperavi/widget.py
@@ -30,25 +30,25 @@ class ImperaviWidget(Textarea):
             'imageGetJson': reverse('imperavi-get-json', kwargs={'upload_path': self.upload_path}),
             'fileUpload': reverse('imperavi-upload-file', kwargs={'upload_path': self.upload_path}),
             'linkFileUpload': reverse('imperavi-upload-link-file', kwargs={'upload_path': self.upload_path}),
-        })
+            })
         imperavi_settings = json.dumps(self.imperavi_settings)
         return mark_safe(u"""
-            <div style="width: 800px;">
+            <div style="width: 100%%;">
                 <textarea%(attrs)s>%(value)s</textarea>
             </div>
             <script>
                 $(document).ready(
                     function() {
                         $("#%(id)s").parent().siblings('label').css('float', 'none');
-                        $("#%(id)s").height(300);
+                        $("#%(id)s").height(100);
                         $("#%(id)s").redactor(%(imperavi_settings)s);
                     }
                 );
             </script>
             """ % {
-                'attrs': flatatt(final_attrs),
-                'value': conditional_escape(force_unicode(value)),
-                'id': field_id,
-                'imperavi_settings': imperavi_settings,
+            'attrs': flatatt(final_attrs),
+            'value': conditional_escape(force_unicode(value)),
+            'id': field_id,
+            'imperavi_settings': imperavi_settings,
             }
         )
